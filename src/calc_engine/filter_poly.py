367efbd07d094500b36ee14d63f6fcb0ec006803
olutosinsonuyi <tosin.sonuyi@gmail.com>
2017-03-20 23:03:54 -0400

    skeleton for text filtering methods

---
+++ src/calc_engine/filter_poly.py
@@ -0,0 +1,66 @@
+import metric_calculations as mc
+
+def filter_by_day_of_week(list_of_days_of_week,tes):
+
+	dict_filtered_tes = {
+	'filtered_tes':None,
+	'filter_criteria':None,
+	'number_returned':None
+	}
+
+
+
+	return dict_filtered_tes
+
+def filter_by_time_of_day(list_of_hours_of_day,tes):
+	dict_filtered_tes = {
+	'filtered_tes':None,
+	'filter_criteria':None,
+	'number_returned':None
+	}
+
+
+	return dict_filtered_tes
+
+def filter_by_month_of_year(list_of_months_of_year,tes):
+	dict_filtered_tes = {
+	'filtered_tes':None,
+	'filter_criteria':None,
+	'number_returned':None
+	}
+
+
+
+	return dict_filtered_tes
+
+def filter_by_day_of_month(list_of_days_of_month,tes):
+	dict_filtered_tes = {
+	'filtered_tes':None,
+	'filter_criteria':None,
+	'number_returned':None
+	}
+
+
+
+	return dict_filtered_tes
+
+def filter_by_year(list_of_years,tes):
+	dict_filtered_tes = {
+	'filtered_tes':None,
+	'filter_criteria':None,
+	'number_returned':None
+	}
+
+
+
+	return dict_filtered_tes
+
+def filter_by_date_range(start_date,end_date,tes):
+	dict_filtered_tes = {
+	'filtered_tes':None,
+	'filter_criteria':None,
+	'number_returned':None
+	}
+
+
+	return dict_filtered_tes
