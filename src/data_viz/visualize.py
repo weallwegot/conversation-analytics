b3bc1a71fd880353f28074511787ebafec97c5f8
olutosinsonuyi <tosin.sonuyi@gmail.com>
2017-04-04 01:10:35 -0400

    ideating visualization scheme

--- src/data_viz/visualize.py
+++ src/data_viz/visualize.py
@@ -1,2 +1,21 @@
 
 
+def create_day_tuples(tes_list):
+	#find integer of first day and last day
+	#partition into bins
+	#if TextEquivalent is between the bins add to count
+	#similar approach can be used for response time trending
+	#checkout numpy and cumsum()
+	"""
+	http://stackoverflow.com/questions/3034162/plotting-a-cumulative-graph-of-python-datetimes
+	http://stackoverflow.com/questions/37293014/draw-a-cumulative-chart-from-a-pandas-dataframe
+	https://docs.scipy.org/doc/numpy/reference/generated/numpy.cumsum.html
+	"""
+	daily_text_eqs = {}
+	for te in tes_list:
+		k =  str(te.date_month) +'-'+ str(te.date_day) +'-'+ str(te.date_year)
+		if(k in daily_text_eqs.keys()):
+			daily_text_eqs[k].append(te)
+		else:
+			daily_text_eqs[k] = [te]
+
