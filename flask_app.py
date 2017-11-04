6a349a5e9d0713edb00aef2cd4a83d4f18592eb8
olutosinsonuyi <tosin.sonuyi@gmail.com>
2017-11-03 22:55:49 -0400

    initial ui commit, nothing to see here

---
+++ flask_app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, url_for, render_template
+from flask import jsonify, session, current_app
+from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS, cross_origin
+import random
+from textblob import TextBlob
+import json
+import requests
+
+#literally should need none of these imports except
+#script 
+#userinfo
+#flask
+#database
+#json
+#uuid
+#datetime
+
+from datetime import datetime
+import simplejson as json
+import uuid
+
+app = Flask(__name__)
+app.secret_key = "2"
+#api
+api = Api(app)
+#cors for cross origin headers 
+CORS(app)
+
+
+@app.route('/')
+def fresh_session():
+    return current_app.send_static_file('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run()
