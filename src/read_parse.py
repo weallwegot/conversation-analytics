5ca97b61ca43eabdf0ef8d87573b3440c78eddae
tosin <tosin.sonuyi@gmail.com>
2018-08-15 16:21:22 -0400

    add comments, identify bug, and input expectations

--- src/read_parse.py
+++ src/read_parse.py
@@ -1,12 +1,17 @@
 import re
-from convo_objects.TextEquivalent import TextEquivalent
+from src.convo_objects.TextEquivalent import TextEquivalent
+
 def read_and_parse_text_file(full_ass_path, block_text_threshold_seconds):
-	#the amount of time between texts for them to be considered 'sequential'
-	#BLOCK_TEXT_THRESHOLD_SECONDS 
 	"""
-	param: full_ass_path :: (str) -> path to the file where the text data is
-	param: block_text_threshold_seconds :: (int) -> number of seconds between sequential texts for 
+	:param full_ass_path: path to the file where the text data is
+	:full_ass_path type: str
+
+	:param block_text_threshold_seconds: number of seconds between sequential texts for 
 	them to be considered as "one" text. they will be merged. see `merge_sequential_text_equiv()`
+	:block_text_threshold_seconds type: int
+
+	:returns: a list of TextEquivalent objects
+	:rtype: list
 	"""
 	text_equivs = []
 
@@ -24,6 +29,7 @@ def read_and_parse_text_file(full_ass_path, block_text_threshold_seconds):
 				
 				timestamp_string = timestamp.group()
 				# slice the raw input such that sender & timestamp are gone.
+				# BUG: this includes the colon and space in the text
 				text_msg = line[len(sender_name):-len(timestamp_string)-1]
 				# get rid of the identifier pipe, first character.
 				te = TextEquivalent(sender_name,timestamp_string[1:],text_msg)
@@ -39,7 +45,5 @@ def read_and_parse_text_file(full_ass_path, block_text_threshold_seconds):
 				else:
 					text_equivs.append(te)
 					i += 1
-			if i == 15:
-				#break
-				pass
+
 	return text_equivs
